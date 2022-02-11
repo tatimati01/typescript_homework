cce73332c5455802a241ce685cd715f8c5f48810
tatimati01 <tati.mati.work@gmail.com>
2022-02-11 13:48:33 +0200

    Initial commit

---
+++ hw_typescript.ts
@@ -0,0 +1,31 @@
+interface IUser {
+    name: string;
+    age: number;
+    gender: string
+}
+
+const user:IUser = {
+    name:"Max",
+    age:18,
+    gender:'male'
+}
+
+
+
+
+function sum(a:number,b:number):number {
+    return a+b
+}
+
+function showSum(a:number,b:number):void {
+    console.log(a + b);
+}
+
+function incAge(someUser:Partial<IUser>, inc:number):Partial<IUser>{
+    someUser.age+=inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2,3)
+console.log(incAge(user, 2))
